8417640d11cbced1ed67812975d3e2279f66305a
Lam <15622383059@163.com>
2019-07-30 18:27:34 +1000

    log to file

--- src/reno.rs
+++ src/reno.rs
@@ -3,6 +3,12 @@ extern crate slog;
 use serde_derive;
 use reqwest::Error;
 
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead};
+use std::time::SystemTime;
+
+pub const LOG_OUTPUT_FILE: &str = "log_output.txt";
+
 use ::{RemoteGenericCongAvoidAlg, NetworkStatus};
 use GenericCongAvoidFlow;
 use GenericCongAvoidMeasurements;
@@ -14,6 +20,7 @@ pub struct Reno {
     cwnd: f64,
 
     last_utilization: f32,
+    log_file: Option<File>,
 }
 
 impl RemoteGenericCongAvoidAlg for Reno {
@@ -34,6 +41,7 @@ impl RemoteGenericCongAvoidAlg for Reno {
             cwnd: f64::from(init_cwnd),
 
             last_utilization: 0.0,
+            log_file: File::create(LOG_OUTPUT_FILE).ok(),
         }
     }
 }
@@ -65,12 +73,11 @@ impl GenericCongAvoidFlow for Reno {
     {
         if network_status.queue_length > 10 {
             println!("Link get full utilized. Decrease cwnd");
-            self.cwnd -= network_status.queue_length as f64 / 10.0;
+            self.cwnd -= f64::from(network_status.queue_length) * f64::from(self.mss);
             return
         }
 
         if network_status.link_utilization == self.last_utilization {
-            println!("Link underutilized. Increase cwnd by 1 / cwnd per packet");
             self.cwnd += f64::from(self.mss) * (f64::from(m.acked) / self.cwnd);
             return
         }
@@ -82,6 +89,13 @@ impl GenericCongAvoidFlow for Reno {
 
         self.cwnd *= 3.0 / (2.0 * link_uti as f64 + 1.0);
         self.last_utilization = network_status.link_utilization;
+
+        write!(self.log_file.as_mut().unwrap(),
+               "time: {:?}\tlink_utilization: {:.2}\tqueue: {}\tcwnd: {}\n",
+               SystemTime::now(),
+               network_status.link_utilization,
+               network_status.queue_length,
+               self.cwnd as u32 / self.mss);
 
 //        let link_uti = network_status.link_utilization * 10.0;
 //        let link_uti = link_uti as u32;
